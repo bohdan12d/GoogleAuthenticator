9b30a4d4e73f7fc64d7c1e7387b3697765ffe79d
Bohdan <137897509+bohdan12d@users.noreply.github.com>
2023-07-16 13:14:14 +0200

    Update GoogleAuthenticator.php

--- GoogleAuthenticator.php
+++ GoogleAuthenticator.php
@@ -6,3 +6,7 @@ class PHPGangsta_GoogleAuthenticator
      * 16 characters, randomly chosen from the allowed base32 characters.
      *
      * @param int $secretLength
+    *
+     * @return string
+     */
+    public function createSecret($secretLength = 16)
