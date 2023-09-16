bd75536922d37777eaed90f57b3aa0bef39b07ca
Bohdan <137897509+bohdan12d@users.noreply.github.com>
2023-09-16 11:27:28 +0200

    Update GoogleAuthenticator.php

--- GoogleAuthenticator.php
+++ GoogleAuthenticator.php
@@ -104,3 +104,6 @@ class PHPGangsta_GoogleAuthenticator
     {
     $width = !empty($params['width']) && (int) $params['width'] > 0 ? (int) $params['width'] : 200;
         $height = !empty($params['height']) && (int) $params['height'] > 0 ? (int) $params['height'] : 200;
+ $level = !empty($params['level']) && array_search($params['level'], array('L', 'M', 'Q', 'H')) !== false ? $params['level'] : 'M';
+
+        $urlencoded = urlencode('otpauth://totp/'.$name.'?secret='.$secret.'');
