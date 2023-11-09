d4186db88180f21fb9d890d3b35af2cc255c0624
Bohdan <137897509+bohdan12d@users.noreply.github.com>
2023-11-09 10:19:08 +0100

    Update GoogleAuthenticator.php

--- GoogleAuthenticator.php
+++ GoogleAuthenticator.php
@@ -24,3 +24,5 @@ if (function_exists('random_bytes')) {
             $rnd = random_bytes($secretLength);
  } elseif (function_exists('mcrypt_create_iv')) {
   $rnd = mcrypt_create_iv($secretLength, MCRYPT_DEV_URANDOM);
+   } elseif (function_exists('openssl_random_pseudo_bytes')) {
+            $rnd = openssl_random_pseudo_bytes($secretLength, $cryptoStrong);
